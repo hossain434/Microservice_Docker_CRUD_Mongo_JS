9e6cf089da4fecda85836fa4c83e0484793edc2b
Md Hossain <arif434@gmail.com>
2020-08-01 21:58:28 -0700

    first commit

---
+++ config/database.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  url:
+    // Without Docker
+    //"mongodb+srv://test:test@cluster.....mongodb.net/test?retryWrites=true&w=majority",
+    //For Docker. Include Dockerfile and docker-compose.yaml
+    "mongodb://mongo:27017/my-test",
+};
